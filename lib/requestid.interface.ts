3969cb9e17feb16b2a03318cec79129c911f84a3
Deokgyu Yang <secugyu@gmail.com>
2023-12-10 20:48:17 +0900

    lib: Use ClsService instead of Request scope that might cause unexpected behavior
    It fixes issue #14
    
    Signed-off-by: Deokgyu Yang <secugyu@gmail.com>

--- lib/requestid.interface.ts
+++ lib/requestid.interface.ts
@@ -1,4 +1,11 @@
+import { ClsStore } from 'nestjs-cls';
 import { RequestIdFormatType } from './requestid.enum';
+
+export interface RequestIdDataStore extends ClsStore {
+  requestId: string;
+  type: RequestIdFormatType;
+  length: number;
+}
 
 export type RequestIdModuleOptions = {
   type?: RequestIdFormatType;
--- lib/requestid.module.ts
+++ lib/requestid.module.ts
@@ -1,33 +1,18 @@
-import { Global, MiddlewareConsumer, Module, Scope } from '@nestjs/common';
-import { REQUEST } from '@nestjs/core';
-import { Request } from 'express';
+import { Global, MiddlewareConsumer, Module } from '@nestjs/common';
+import { ClsModule } from 'nestjs-cls';
 import { REQUEST_ID_TOKEN } from './requestid.constants';
-import {
-  ConfigurableModuleClass,
-  MODULE_OPTIONS_TOKEN,
-} from './requestid.module-definition';
+import { ConfigurableModuleClass } from './requestid.module-definition';
 import { RequestIdMiddleware } from './requestid.middleware';
 import { RequestIdService } from './requestid.service';
-import { RequestIdModuleOptions } from './requestid.interface';
-import { RequestIdFormatType } from './requestid.enum';
 
 const requestIdServiceProvider = {
   provide: REQUEST_ID_TOKEN,
-  useFactory: (options: RequestIdModuleOptions, request: Request) =>
-    new RequestIdService(
-      {
-        // Set default values for the options if they are not provided
-        type: options?.type ?? RequestIdFormatType.UUID_V4,
-        length: options?.length ?? 21, // Default size for nanoid,
-      },
-      request,
-    ),
-  inject: [MODULE_OPTIONS_TOKEN, REQUEST],
-  scope: Scope.REQUEST,
+  useClass: RequestIdService,
 };
 
 @Global()
 @Module({
+  imports: [ClsModule.forFeature()],
   providers: [requestIdServiceProvider],
   exports: [requestIdServiceProvider],
 })
